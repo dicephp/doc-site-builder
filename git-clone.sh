82ee5180722ea9b36b06a077d058b9490afbe121
Vaibhav Kaushal <vaibhav@plackal.in>
2018-04-23 10:17:42 +0530

    Attempting the cloning command

---
+++ git-clone.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git clone https://github.com/dicephp/types.git -d tt
--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 require_once 'config.php';
 
+//Get the repo name sent in the webhook
+
+
+$response = [];
+$cloneReturn = 0;
+
+$t = exec("git-clone.sh", $response, $cloneReturn);
+//$t = exec("ls -lah", $response, $cloneReturn);
+
+foreach ($response as $line) {
+    echo $line;
+}
+
+var_dump(time());
+var_dump($response);
+var_dump($cloneReturn);
+
+phpinfo();
